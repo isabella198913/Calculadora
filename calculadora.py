525a032ba264e89597176bc7da26518f9e6cf806
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-08-29 17:20:25 -0500

    potencia

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,9 @@ print("4. division")
 print("5. potencia")
 
 x= int(input("seleccione una opcion del 1 al 5"))
+if x==5:
+    print("potencia")
+    a= int(input("ingrese la base de la potencia"))
+    b= int(input("ingrese el exponente de la potencia"))
+    print("el resultado es",a**b)
+    
