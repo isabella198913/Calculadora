886568e4e705d561ddaa4c47fc0e779f107eeea2
“AndresGutierrezHurtado” <andres52885241@gmail.com>
2025-08-29 16:40:43 -0500

    Menu de opciones

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+# Calculadora
+print("Menu de opciones")
+print("1. suma")
+print("2. resta")
+print("3. multiplicacion")
+print("4. division")
+print("5. potencia")
+
+x= int(input("seleccione una opcion del 1 al 5"))
