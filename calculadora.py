4e1743821feb3fe0a277f136b1ad21b36660ab36
juuansebastiarBG <juuansebastianbernalgamboa@gmail.com>
2025-08-29 17:26:28 -0500

    Division

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,14 @@ print("4. division")
 print("5. potencia")
 
 x= int(input("seleccione una opcion del 1 al 5"))
+
+#Mutiplicacion
+if x==4:
+    print("Haz selecconado la opcion division")
+    a= int (input("ingrese el primer numero"))
+    b= int (input("ingrese el segundo numero"))
+    print("el resultad es:",a/b)
+
+
+    
+    
