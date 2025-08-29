d7e052c187fd140a0e09165fa31006dc3061cd1e
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-08-29 17:06:10 -0500

    multiplicación

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,9 @@ print("4. division")
 print("5. potencia")
 
 x= int(input("seleccione una opcion del 1 al 5"))
+
+while x == 3:
+    print("ingrese dos numeros")
+    a= int(input())
+    b= int(input())
+    print("el resultado es", a*b)
