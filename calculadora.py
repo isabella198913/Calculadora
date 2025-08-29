2ae2cb1a57d4e2cb7f90a01943ffa27785682ccc
juuansebastiarBG <juuansebastianbernalgamboa@gmail.com>
2025-08-29 17:08:12 -0500

    Suma

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,8 @@ print("4. division")
 print("5. potencia")
 
 x= int(input("seleccione una opcion del 1 al 5"))
+if x == 1:
+    a= int(input("ingrese el primer número"))
+    b= int(input("ingrese el segundo número"))
+    print("el resutado es: ", a+b)
+
